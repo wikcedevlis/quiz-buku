c7c58507c5ca62250be4c31129fcecda6925b61c
wikcedevlis <mueeekkz@gmail.com>
2025-01-09 00:02:38 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 document.querySelector("form").addEventListener("submit", function(e) {
-    e.preventDefault();
+    e.preventDefault(); // Menghentikan form agar tidak reload halaman
+
     let selectedOption = document.querySelector('input[name="world"]:checked');
     if (selectedOption) {
+        // Menampilkan hasil berdasarkan pilihan pengguna
         alert("Kamu adalah seorang petualang dalam dunia " + selectedOption.value + "!");
     } else {
         alert("Pilih satu dunia untuk melanjutkan.");
     }
 });
+
 
